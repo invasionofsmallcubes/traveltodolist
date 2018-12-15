f2191cc0017fbaf675853f4d3f445d16e3622979
Emanuele Ianni <dierre@gmail.com>
2018-12-15 10:26:07 +0100

    initial commit
    Solved using this escamotage to separate frontend from backend: http://blog.aliprax.me/nodejs-proxy-react-app/

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var compression = require('compression');
+var httpProxy = require('http-proxy');
+var API_HOST = process.env.API_HOST || 'localhost'
+var PORT = process.env.SERVER_PORT || 5000;
+
+// Initialize
+var app = express();
+var apiProxy = httpProxy.createProxyServer();
+
+// Serve static resources from 'build' folder
+app.use(express.static(__dirname + '/client/build'));
+
+// Enable gzip response compression
+app.use(compression());
+
+// Proxy all the api requests
+app.all('/api/*', function (req, res) {
+  apiProxy.web(req, res, { target: 'http://' + API_HOST })
+});
+
+// Otherwise serve index.html
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
+app.listen(PORT);
+
+console.log('Running on port ' + PORT + ' with API_HOST:' + API_HOST);
+
+
